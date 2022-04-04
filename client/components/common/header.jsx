bc2a7d3db7d4172b0a3e82b7d6f459749a94eb15
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-04-03 22:34:28 -0600

    Almost forgot to add a home function

--- client/components/common/header.jsx
+++ client/components/common/header.jsx
@@ -1,14 +1,16 @@
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 import { Room } from "../home/room";
 
-export const Header =({ logout, name }) => {
+export const Header =({ logout, home }) => {
   const navigate = useNavigate();
   return(
     <div className="header">
       <h1 className="title">GeoChat</h1>
       <h2 className="coordTag">Why talk to the people nearby when you can just chat them virtually?</h2>
       <div className="exit-div">
-        <button onClick={logout} className="exit">logout</button>
+        <button onClick={logout} className="exit-home">logout</button>
+        <Link to={'/'}><button className="exit-home">home</button></Link>
       </div>
     </div>
   )
