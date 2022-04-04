82b217c3386d446b9898e8124db526c5d7159a00
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-04-03 23:08:33 -0600

    Fix title

--- client/components/common/header.jsx
+++ client/components/common/header.jsx
@@ -6,7 +6,7 @@ export const Header =({ logout, home }) => {
   const navigate = useNavigate();
   return(
     <div className="header">
-      <h1 className="title">GeoChat</h1>
+      <h1 className="title">GeoChats</h1>
       <h2 className="coordTag">Why talk to the people nearby when you can just chat them virtually?</h2>
       <div className="exit-div">
         <button onClick={logout} className="exit-home">logout</button>
